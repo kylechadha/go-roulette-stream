f1a90c169ecefa74960a5f05681990b53d63e18b
Kyle Chadha <kyle.chadha@gmail.com>
2015-12-06 11:05:31 -0500

    Add SimplePeer lib and SocketServer constructor

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,6 +3,47 @@ navigator.getUserMedia = navigator.getUserMedia ||
 	navigator.mozGetUserMedia ||
 	navigator.msGetUserMedia;
 
+
+var SocketServer = function(url) {
+	var conn = new WebSocket(url);
+	var callbacks = {};
+
+	this.bind = function(event_name, callback) {
+		callbacks[event_name] = callbacks[event_name] || [];
+		callbacks[event_name].push(callback);
+		return this;
+	};
+
+	this.send = function(event_name, event_data) {
+		var payload = JSON.stringify({
+			event: event_name,
+			data: event_data
+		});
+		conn.send(payload);
+		return this;
+	};
+
+	conn.onmessage = function(evt) {
+		var json = JSON.parse(evt.data)
+		dispatch(json.event, json.data)
+	};
+
+	conn.onclose = function() {
+		dispatch('close', null)
+	}
+	conn.onopen = function() {
+		dispatch('open', null)
+	}
+
+	var dispatch = function(event_name, message) {
+		var chain = callbacks[event_name];
+		if (typeof chain == 'undefined') return; // no callbacks for this event
+		for (var i = 0; i < chain.length; i++) {
+			chain[i](message)
+		}
+	}
+};
+
 $(document).ready(function() {
 
 	// If WebSockets are not available, no dice.
@@ -10,7 +51,27 @@ $(document).ready(function() {
 		return;
 	}
 
-	conn = new WebSocket('ws://' + window.location.host + '/ws');
+	var goServer = new SocketServer('ws://' + window.location.host + '/ws');
+
+
+	var $messages = $('#messages'),
+		$modal = $('#name-prompt').modal({
+			backdrop: 'static'
+		}),
+		currentUser;
+
+	$modal.find('button').click(function(e) {
+		e.preventDefault();
+		name = $modal.find('input').val().trim();
+
+		if (name === '') return;
+
+		console.log("Kicking it all off.")
+		$modal.modal('hide');
+
+		initiate(name);
+	});
+
 
 	var mediaOptions = {
 		audio: true,
@@ -22,43 +83,66 @@ $(document).ready(function() {
 		}
 	};
 
-	var $messages = $('#messages'),
-		$modal = $('#name-prompt').modal({
-			backdrop: 'static'
-		})
+	var initiate = function(name) {
+		console.log("Initiate: starting webcam.")
+		currentUser = {
+			name: name,
+			// id: guid(),
+			// stream: undefined
+		};
 
-	$modal.find('button').click(function(e) {
-		e.preventDefault();
-		name = $modal.find('input').val().trim();
-		if (name === '') return;
-
-		console.log("Kicking it all off.")
-		$modal.modal('hide');
-	});
-
-	var video = document.querySelector('video');
-
-	if (navigator.getUserMedia) {
-		// Note: Can set additional constraints here (video: {mandatory: { minWidth: 1280, minHeight: 720 }})
-		navigator.getUserMedia({
-			audio: true,
-			video: true
-		}, function(stream) {
-			console.log("stream");
-			video.src = window.URL.createObjectURL(stream);
-
-			console.log(window.URL.createObjectURL(stream));
-
-			video.onloadedmetadata = function(e) {
-				console.log("onloadedmetadata");
-				// Ready to go. Do some stuff.
-			};
-		}, function(e) {
+		navigator.getUserMedia(mediaOptions, connect, function(e) {
 			console.error('Error using getUserMedia', e);
 		});
-	} else {
-		alert("Your browser does not support navigator.getUserMedia. No fun for you :(.")
 	}
+
+	var initiator = false;
+	goServer.onmessage = function(m) {
+		if (m.data === "initiator") {
+			initiator = true;
+		}
+	};
+
+	var connect = function(stream) {
+		console.log("getUserMedia: got stream.")
+		var video = $('#localVideo')[0];
+		video.src = window.URL.createObjectURL(stream);
+
+		var peer = new SimplePeer({
+			initiator: initiator,
+			stream: stream,
+			trickle: false
+		});
+
+		peer.on('signal', function(data) {
+			// when peer1 has signaling data, give it to peer2
+			goServer.send(data)
+		})
+	}
+
+	// var video = document.querySelector('video');
+
+	// if (navigator.getUserMedia) {
+	// 	// Note: Can set additional constraints here (video: {mandatory: { minWidth: 1280, minHeight: 720 }})
+	// 	navigator.getUserMedia({
+	// 		audio: true,
+	// 		video: true
+	// 	}, function(stream) {
+	// 		console.log("stream");
+	// 		video.src = window.URL.createObjectURL(stream);
+
+	// 		console.log(window.URL.createObjectURL(stream));
+
+	// 		video.onloadedmetadata = function(e) {
+	// 			console.log("onloadedmetadata");
+	// 			// Ready to go. Do some stuff.
+	// 		};
+	// 	}, function(e) {
+	// 		console.error('Error using getUserMedia', e);
+	// 	});
+	// } else {
+	// 	alert("Your browser does not support navigator.getUserMedia. No fun for you :(.")
+	// }
 
 
 	// // If WebSockets are not available, no dice.
